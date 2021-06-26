e6230031fb15cd96f50ccb5289307adcd2f60173
raj-xe <dhananirajr@gmail.com>
2021-06-26 15:12:29 -0500

    finish primary tasks

--- src/main/java/A1/Q2.java
+++ src/main/java/A1/Q2.java
@@ -2,6 +2,7 @@ package A1;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 import javax.swing.*;
 
 class BlockConfig {
@@ -18,34 +19,227 @@ class BlockConfig {
     }
 }
 
-public class Q2{
+class SingleBlockConfig {
+    int colorType;
+    int x;
+    int y;
+
+    @Override
+    public String toString() {
+        return "SingleBlockConfig{" +
+                "colorType=" + colorType +
+                ", x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+    public SingleBlockConfig(int colorType, int x, int y) {
+        this.colorType = colorType;
+        this.x = x;
+        this.y = y;
+    }
+
+    public SingleBlockConfig setColorType(int colorType) {
+        this.colorType = colorType;
+        return this;
+    }
+
+    public SingleBlockConfig setX(int x) {
+        this.x = x;
+        return this;
+    }
+
+    public SingleBlockConfig setY(int y) {
+        this.y = y;
+        return this;
+    }
+}
+
+
+public class Q2 {
 
     final static Boolean[] isPaused = {false};
-    final static BlockConfig[] blocks = new BlockConfig[] {
-        new BlockConfig(1,1, (int) (Math.random() * 10), (int) (Math.random() * 10)),
-        new BlockConfig(4,19, 0, (int) (Math.random() * 10)),
-        new BlockConfig(12,1, (int) (Math.random() * 10), (int) (Math.random() * 10)),
-    };
+
+    static JFrame frame;
+
+    static BlockConfig[] blocks;
+
+    static int cx, cy;
+
+    static BlockConfig currentBlock, nextBlock;
+
+    static Graphics g;
+
+    static float single_unit;
+
+    static ArrayList<SingleBlockConfig> occupied;
+
+    static void init() {
+        cx = 3;
+        cy = 2;
+    }
+
+    static int currentType, nextType;
+
+    static boolean toLeft, toRight;
+
+    static class SubThread implements Runnable {
+
+        Thread dropThread;
+
+        public SubThread() {
+            this.dropThread = new Thread(this,"Drop the block thread");
+        }
+
+        @Override
+        public void run() {
+            // GameLoop
+            while(true){
+                try{
+
+                    if(isPaused[0]) {
+                        dropThread.sleep(200);
+                        continue;
+                    }
+
+                    if(currentBlock == null) {
+                        System.out.println("current block is null");
+                        continue;
+                    }
+
+                    if(isBlockCollidingNow()) {
+                        int[][] coords = Mapper.getByType(currentBlock.type);
+                        for (
+                                int[] c: coords
+                            ) {
+                            occupied.add(new SingleBlockConfig(currentBlock.colorType, cx+c[0], cy+c[1]));
+                        }
+                        reInit();
+                        continue;
+                    }
+
+
+                    if(toLeft) {
+                        int[][] coords = Mapper.getByType(currentBlock.type);
+                        boolean flg = false;
+                        for (int[] c:
+                                coords) {
+                            if((occupied != null && occupied.stream().anyMatch(block -> (block.x <= c[0] + cx - 1 && block.y ==  c[1] + cy + 1)))){
+                                flg = true;
+                            }
+                        }
+
+                        if(cx > 0 && !flg)
+                            cx--;
+                        toLeft = false;
+                    } else if(toRight) {
+                        int[][] coords = Mapper.getByType(currentBlock.type);
+                        boolean flg = false;
+                        for (int[] c:
+                                coords) {
+                            if(c[0] + cx >= 9 || (occupied != null && occupied.stream().anyMatch(block -> (block.x >= c[0] + cx + 1 && block.y ==  c[1] + cy + 1)))){
+                                flg = true;
+                            }
+                        }
+
+                        if(cx < 9 && !flg)
+                            cx++;
+                        toRight = false;
+                    }
+
+                    cy++;
+
+                    dropThread.sleep(1000);
+
+                    frame.repaint();
+
+                    if(g != null)
+                        draw(g,single_unit);
+
+                } catch (Exception e) {
+                    System.out.println("ERROR IN DROP THREAD:"+e);
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        boolean isBlockCollidingNow(){
+            int[][] coords = Mapper.getByType(currentBlock.type);
+            int cy_next = cy+1;
+            for (int[] c:
+                 coords) {
+                if((occupied != null && occupied.stream().anyMatch(block -> (block.x == c[0]+cx && block.y ==  c[1]+cy_next))) || c[1] + cy_next > 19){
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void start() {
+            dropThread.start();
+            System.out.println(dropThread);
+            System.out.println("starting a sub thread: drop thread");
+        }
+    }
+
+    public static void reInit(){
+        cx = 3;
+        cy = 2;
+        currentType = nextType;
+        nextType = (int) (Math.random() * 10);
+        frame.repaint();
+    }
+
+
 
     public static void main(String[] args) {
+        occupied = new ArrayList<>();
         final float ratio = (float) (1.7/2);
         int height = 1000;
         Dimension d = new Dimension((int) (height * ratio),height);
-        JFrame frame = new JFrame();
+        frame = new JFrame();
         frame.setSize(d);
         frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+
+        toLeft = false;
+        toRight = false;
+
+        init();
+
+        currentType = (int) (Math.random() * 10);
+        nextType= (int) (Math.random() * 10);
+
+        SubThread dropThread = new SubThread();
+        dropThread.start();
 
         JPanel panel = new JPanel() {
 
-            protected void paintComponent(Graphics g) {
+            protected void paintComponent(Graphics g_) {
 
+                blocks = new BlockConfig[] {
+                        new BlockConfig(12,1, nextType, (int) nextType),
+                };
 
-                var single_unit = getHeight() / 20;
+                currentBlock =
+                        new BlockConfig(cx,cy, currentType, currentType);
+
+                single_unit = (float) getHeight() / 20;
                 int single_unit_int = (int) single_unit;
                 this.setSize((int) (getHeight() * ratio + 10), getHeight());
 
+                g = g_;
 
                 draw(g, single_unit);
+
+                occupied.forEach(block -> {
+                    Color prevColor = g.getColor();
+                    g.setColor(Mapper.getColorByType(block.colorType));
+                    g.fillRect((block.x) * single_unit_int,(block.y) * single_unit_int, single_unit_int, single_unit_int);
+                    g.setColor(prevColor);
+                    g.drawRect((block.x) * single_unit_int,(block.y) * single_unit_int, single_unit_int, single_unit_int);
+                });
+
 
                 if(isPaused[0]) {
                     Color prev = g.getColor();
@@ -76,14 +270,27 @@ public class Q2{
                 MouseAdapter q = new MouseAdapter() {
                     @Override
                     public void mouseClicked(MouseEvent e) {
-                        System.out.println(e);
+                        // for exit button
                         if(
                                 e.getX() >= (single_unit_int * 11)
-                                && e.getY() >= (single_unit_int * 16.25)
+                                && e.getY() >= (single_unit_int * 15.8)
                                 && e.getX() <= (single_unit_int * 13.8)
                                 && e.getY() <= (single_unit_int * 17.25)
                         ) {
                             System.exit(0);
+                        }
+
+                        if(isPaused[0]) {
+                            return;
+                        }
+
+                        // for moving the block on left or right
+                        if(e.getButton() == 1) {
+                            // move left
+                            toLeft = true;
+                        } else if(e.getButton() == 3) {
+                            // move right
+                            toRight = true;
                         }
                     }
                 };
@@ -140,20 +347,23 @@ public class Q2{
             new TetrisBlock(config.x, config.y, config.type, config.colorType, g, (int) single_unit);
         }
 
+        new TetrisBlock(currentBlock.x, currentBlock.y, currentBlock.type, currentBlock.colorType, g, (int) single_unit);
+
     }
 }
 
-class TetrisBlock {
-    int x, y, type, colorType;
-    private int[][][] coords = {
+class Mapper {
+    static private int[][][] coords = {
             {{0,0}, {1,0}, {2,0}, {3,0}}, // linear horizontal
-            {{0,0}, {0,1}, {0,2}, {0,3}}, // linear vertical
+            {{0,0}, {0,1}, {1,1}, {2,1}}, // L shape - v1
+            {{0,2}, {0,1}, {1,1}, {2,1}}, // L shape - v2
             {{0,0}, {0,1}, {1,0}, {1,1}}, // square
-            {{0,0}, {0,1}, {1,1}, {1,2}}, // zig-zag left - top to bottom right
-            {{1,0}, {1,1}, {0,1}, {0,2}}, // zig-zag right - top to bottom left
-            {{0,0}, {1,0}, {2,0}, {1,1}}, // T shaped
+            {{1,0}, {2,0}, {0,1}, {1,1}}, // zig-zag left - top to bottom right
+            {{0,0}, {1,0}, {1,1}, {2,1}}, // zig-zag right - top to bottom left
+            {{0,1}, {1,0}, {2,1}, {1,1}}, // T shaped
     };
-    private Color[] colors = {
+
+    static Color[] colors = {
             Color.BLUE,
             Color.RED,
             Color.GREEN,
@@ -161,6 +371,28 @@ class TetrisBlock {
             Color.CYAN,
             Color.orange
     };
+
+    static int[][][] getAll() {
+        return coords;
+    }
+
+    static int[][] getByType(int type) {
+        return coords[type % coords.length];
+    }
+
+    static Color[] getAllColors(){
+        return colors;
+    }
+
+    static Color getColorByType(int type) {
+        return colors[type % colors.length];
+    }
+}
+
+class TetrisBlock {
+    int x, y, type, colorType;
+
+
     private Color color;
     Graphics g;
     int single_unit;
@@ -168,16 +400,16 @@ class TetrisBlock {
     public TetrisBlock(int x, int y, int type, int colorType, Graphics g, int single_unit) {
         this.x = x;
         this.y = y;
-        this.type = type % coords.length;
-        this.colorType = colorType % colors.length;
-        this.color = colors[this.colorType];
+        this.type = type % Mapper.getAll().length;
+        this.colorType = colorType % Mapper.getAllColors().length;
+        this.color = Mapper.getColorByType(this.colorType);
         this.g = g;
         this.single_unit = single_unit;
         draw();
     }
 
     public void draw(){
-        for(int[] point : coords[type]){
+        for(int[] point : Mapper.getByType(type)){
             int x_ = point[0], y_ = point[1];
             Color prevColor = g.getColor();
             g.setColor(color);
